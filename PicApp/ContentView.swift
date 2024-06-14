142cd9283592aec0dc186b9f36aec9cc5cc49a9e
Negin Zahedi <zahedi.negin@gmail.com>
2024-06-14 12:42:21 -0400

    Initial Commit

---
+++ PicApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PicApp
+//
+//  Created by Negin Zahedi on 2024-06-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PicApp/PicAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PicAppApp.swift
+//  PicApp
+//
+//  Created by Negin Zahedi on 2024-06-14.
+//
+
+import SwiftUI
+
+@main
+struct PicAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
