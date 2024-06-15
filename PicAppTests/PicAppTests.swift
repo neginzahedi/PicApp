aff3e717188089e4653cb9188083400c593e9ee5
Negin Zahedi <zahedi.negin@gmail.com>
2024-06-15 16:31:21 -0400

    feat: Add unit tests for capitalizedEachWord extension

--- PicAppTests/PicAppTests.swift
+++ PicAppTests/PicAppTests.swift
@@ -18,18 +18,27 @@ final class PicAppTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testCapitalizedEachWord() throws {
+        // Test empty string
+        XCTAssertEqual("".capitalizedEachWord(), "")
+        
+        // Test multiple word
+        XCTAssertEqual("a large group of round white objects".capitalizedEachWord(), "A Large Group Of Round White Objects")
+        
+        // Test words with different casing
+        XCTAssertEqual("a lArge group of roUnd whIte obJects".capitalizedEachWord(), "A LArge Group Of RoUnd WhIte ObJects")
+        
+        // Test multiple spaces
+        XCTAssertEqual("   a large group    of round white objects".capitalizedEachWord(), "   A Large Group    Of Round White Objects")
+        
+        // Test non alphabetic characters
+        XCTAssertEqual("123 round white objects".capitalizedEachWord(), "123 Round White Objects")
     }
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
-            // Put the code you want to measure the time of here.
+            _ = "a large group of round white objects".capitalizedEachWord()
         }
     }
 
